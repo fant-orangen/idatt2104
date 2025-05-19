ee119587271ca8860fe063b448fa22eb7f00683a
Josefine Arntsen <josefia@ulrik.uio.no>
2025-05-19 13:52:06 +0200

    feat: updated header files for udp socket implementation

--- include/netcode/server.hpp
+++ include/netcode/server.hpp
@@ -1,14 +1,25 @@
 #pragma once
 #include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 class Server {
 public:
     Server(int port);
+    ~Server();
+
     bool start();
     void stop();
     bool is_running() const;
 
+    bool send_data(const void* data, size_t size, const struct sockaddr_in& client_addr);
+    int receive_data(void* data, size_t buffer_size, struct sockaddr_in& client_addr);
+
 private:
     int port_;
     bool running_;
+    int socket_fd_;
+    struct sockaddr_in server_addr_;
 }; 
