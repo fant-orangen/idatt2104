49666b1dd8608a04af348dcf7961ec492ecf2fb0
Josefine Arntsen <josefia@ulrik.uio.no>
2025-05-20 17:08:09 +0200

    feat: made player state serializer files

--- include/netcode/serialization/player_state_serializer.hpp
+++ include/netcode/serialization/player_state_serializer.hpp
@@ -4,23 +4,8 @@
 
 namespace netcode::serialization {
 
-    inline void serialize(Buffer& buffer, const packets::PlayerStatePacket& packet) {
-        buffer.write(packet.player_id);
-        buffer.write(packet.x);
-        buffer.write(packet.y);
-        buffer.write(packet.z);
-        buffer.write(packet.velocity_y);
-        buffer.write(packet.is_jumping);
-    }
+    bool serialize(Buffer& buffer, const packets::PlayerStatePacket& packet);
 
-    inline bool deserialize(Buffer& buffer, packets::PlayerStatePacket& packet) {
-        if (!buffer.read(packet.player_id)) return false;
-        if (!buffer.read(packet.x)) return false;
-        if (!buffer.read(packet.y)) return false;
-        if (!buffer.read(packet.z)) return false;
-        if (!buffer.read(packet.velocity_y)) return false;
-        if (!buffer.read(packet.is_jumping)) return false;
-        return true;
-    }
+    bool deserialize(Buffer& buffer, packets::PlayerStatePacket& packet);
 
 }
