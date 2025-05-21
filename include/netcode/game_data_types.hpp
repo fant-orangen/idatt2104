939062a847b9c6e07c612a6594270ed85eb016f2
Magnus Eik <mageik@stud.ntnu.no>
2025-05-21 15:14:13 +0200

    feat(vector): update game_data_types.hpp

--- include/netcode/game_data_types.hpp
+++ include/netcode/game_data_types.hpp
@@ -1,12 +1,12 @@
 #ifndef GAME_DATA_TYPES_HPP
 #define GAME_DATA_TYPES_HPP
 
-#include "raylib.h"
+#include "netcode/math/my_vec3.hpp"
 #include <cstdint>
 
 struct PlayerInput {
     uint32_t frame_number;
-    Vector3 movement_input_vector;
+    MyVec3 movement_input_vector;
     bool jump_action_triggered;
 };
 
@@ -14,9 +14,15 @@ struct PlayerFrameState {
     uint32_t frame_number;
     uint32_t player_id;
 
-    Vector3 position;
-    Vector3 velocity;
+    MyVec3 position;
+    MyVec3 velocity;
     bool is_jumping_state;
+    bool states_match(const PlayerFrameState& other) const {
+        return frame_number == other.frame_number &&
+               player_id == other.player_id &&
+               position == other.position &&
+               velocity == other.velocity &&
+               is_jumping_state == other.is_jumping_state;
 };
 
 #endif
