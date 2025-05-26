5baaf29faf330e79e090f50e80eb63ad7bedd960
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-05-26 09:23:35 +0200

    refactor: Remove unnecessary files.

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/havardvd/git/NTNU/IDATT2104/Project
-# Build directory: /Users/havardvd/git/NTNU/IDATT2104/Project/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/havardvd/git/NTNU/IDATT2104/Project/cmake-build-debug/netcode_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
